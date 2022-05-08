6fe4511e8b04321cce0c999b4264df9e782adbed
Russell <halfbcsina@yahoo.com.sg>
2022-05-08 09:34:33 +0800

    controller wip + workflow test

--- src/main/java/com/vttp/miniproject/Project/controllers/SearchController.java
+++ src/main/java/com/vttp/miniproject/Project/controllers/SearchController.java
@@ -1,8 +1,20 @@
 package com.vttp.miniproject.Project.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SearchController {
-    
+    @GetMapping(path = "/results")
+    public ModelAndView keywordSearch(@RequestParam String keyword){
+        return null;
+    }
+
+    @PostMapping(path = "/results")
+    public ModelAndView chooseForMe(){
+        return null;
+    }
 }
