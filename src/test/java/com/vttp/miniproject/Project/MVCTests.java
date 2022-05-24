8c1f19238d92dd27f099def8ac2b4299939366e0
relight31 <halfbcsina@yahoo.com.sg>
2022-05-24 12:57:27 +0800

    tests

--- src/test/java/com/vttp/miniproject/Project/MVCTests.java
+++ src/test/java/com/vttp/miniproject/Project/MVCTests.java
@@ -44,6 +44,14 @@ public class MVCTests {
     }
 
     @Test
+    void directAccessWithLogin() {
+        MockHttpServletResponse resp = getRespWithLogin(
+                true,
+                "/auth/search");
+        assertEquals(200, resp.getStatus());
+    }
+
+    @Test
     void accessWithoutLogin() {
         MockHttpServletResponse resp = getRespWithLogin(
                 false,
@@ -113,11 +121,19 @@ public class MVCTests {
     // test individual listing
 
     @Test
-    void listingView() {
+    void listingViewPass() {
         MockHttpServletResponse resp = getRespWithLogin(
                 true,
                 "/auth/listing/00526f9f48db7484243b5c09d2d1daa09ff");
         assertEquals(200, resp.getStatus());
+    }
+
+    @Test
+    void listingViewFail() {
+        MockHttpServletResponse resp = getRespWithLogin(
+                true,
+                "/auth/listing/1");
+        assertEquals(400, resp.getStatus());
     }
 
     @Test
@@ -126,5 +142,48 @@ public class MVCTests {
                 true,
                 "/auth/favourites");
         assertEquals(200, resp.getStatus());
+    }
+
+    @Test
+    void postCommentFailNoBody() {
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("username", "test");
+        session.setAttribute("uuid", "00526f9f48db7484243b5c09d2d1daa09ff");
+
+        RequestBuilder req = MockMvcRequestBuilders.post("/auth/listing/00526f9f48db7484243b5c09d2d1daa09ff")
+                .accept(MediaType.TEXT_HTML)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("commentBody", "")
+                .param("rating", "5")
+                .session(session);
+        MvcResult result = null;
+        try {
+            result = mockMvc.perform(req).andReturn();
+        } catch (Exception e) {
+            fail("Cannot perform mock request", e);
+        }
+        MockHttpServletResponse resp = result.getResponse();
+        assertEquals(400, resp.getStatus());
+    }
+
+    @Test
+    void addToFavouritesFailAlreadyInside() {
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("username", "test");
+        session.setAttribute("uuid", "00526f9f48db7484243b5c09d2d1daa09ff");
+
+        RequestBuilder req = MockMvcRequestBuilders.post("/auth/favourites")
+                .accept(MediaType.TEXT_HTML)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("uuid", "00526f9f48db7484243b5c09d2d1daa09ff")
+                .session(session);
+        MvcResult result = null;
+        try {
+            result = mockMvc.perform(req).andReturn();
+        } catch (Exception e) {
+            fail("Cannot perform mock request", e);
+        }
+        MockHttpServletResponse resp = result.getResponse();
+        assertEquals(400, resp.getStatus());
     }
 }
